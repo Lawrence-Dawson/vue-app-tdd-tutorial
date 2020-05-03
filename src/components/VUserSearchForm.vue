df0f8549120bc72ef1f400cb0f8d5a9bb2cbe257
Lawrence Dawson <lawrence.dawson@sainsburys.co.uk>
2020-05-03 17:29:14 +0100

    finish section 4

--- src/components/VUserSearchForm.vue
+++ src/components/VUserSearchForm.vue
@@ -11,7 +11,19 @@ export default {
 
 <template>
     <form @submit.prevent="$emit('submitted', username)">
+        <el-input
+          placeholder="Pesquise o usuario"
+          v-model="username"
+          class="search-form__input"
+        >
+            <el-button
+            class="search-form__button"
+            native-type="submit"
+            slot="append"
+            icon="el-icon-search"
+            ></el-button>
+        </el-input>
         <input type="text" v-model="username" />
-        <button type="submit">Enviar</button>
+        <button type="submit">Search</button>
     </form>
 </template>
